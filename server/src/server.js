ef384e6963565b073d327c844d6fdd09039af8de
ryandougc <ryan.carr@email.kpu.ca>
2023-09-02 15:51:17 -0700

    Removed socket.io admin server sertup

--- server/src/server.js
+++ server/src/server.js
@@ -2,17 +2,19 @@ import 'dotenv/config.js'
 
 // Socket.io Server
 import { Server } from 'socket.io'
-import { instrument as initSocketIoAdminDashboard } from '@socket.io/admin-ui'
 import initSockets from './lib/sockets.js'
 
 const SOCKET_PORT = process.env.SOCKET_PORT || 3001
 
 const io = new Server(SOCKET_PORT, {
     cors: {
-        origin: ['https://admin.socket.io', process.env.FRONT_END_URL],
+        origin: [/*'https://admin.socket.io',*/ process.env.FRONT_END_URL],
         credentials: true
     }
 })
 
 initSockets(io)
-initSocketIoAdminDashboard(io, { auth: false })
+
+// // Socket.io admin server
+// import { instrument as initSocketIoAdminDashboard } from '@socket.io/admin-ui'
+// initSocketIoAdminDashboard(io, { auth: false })
