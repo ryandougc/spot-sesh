f9ba22ab7eabe3c2c0d7b14a82a5b8df592954c6
ryandougc <ryan.carr@email.kpu.ca>
2023-09-22 17:24:17 -0700

    Enabled express server to host vue app

--- server/src/server.js
+++ server/src/server.js
@@ -1,26 +1,26 @@
 import 'dotenv/config.js'
 
-// // Express server
-// import express from 'express'
+// Express server
+import express from 'express'
 
-// const app = express()
+const app = express()
 
-// const EXPRESS_PORT = process.env.EXPRESS_PORT || 8080
-// app.use(express.static('public'))
+const EXPRESS_PORT = process.env.EXPRESS_PORT || 8080
+app.use(express.static('public'))
 
-// app.get("*", (req, res, next) => {
-//     res.sendFile('index.html', { root: '/home/ryan/Documents/Web Dev/spotify-sesh/server/public' })
-// })
+app.get("*", (req, res, next) => {
+    res.sendFile('index.html', { root: '/home/ryan/Documents/Web Dev/spotify-sesh/server/public' })
+})
 
-// app.get("/*", (req, res, next) => {
-//     res.sendFile('index.html', { root: '/home/ryan/Documents/Web Dev/spotify-sesh/server/public' })
-// })
+app.get("/*", (req, res, next) => {
+    res.sendFile('index.html', { root: '/home/ryan/Documents/Web Dev/spotify-sesh/server/public' })
+})
 
-// app.listen(EXPRESS_PORT, (err) => {
-//     if(err) return console.log(err)
+app.listen(EXPRESS_PORT, (err) => {
+    if(err) return console.log(err)
 
-//     return console.log(`Server is listening on port ${EXPRESS_PORT}`)
-// })
+    return console.log(`Server is listening on port ${EXPRESS_PORT}`)
+})
 
 // Socket.io Server
 import { Server } from 'socket.io'
