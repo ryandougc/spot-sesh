524ed99ccd3edf1d5fda59c0676029aa554f3247
ryandougc <ryandougc@gmail.com>
2023-10-10 15:48:14 -0700

    Created the front-end error service

---
+++ vue/src/services/ErrorService.js
@@ -0,0 +1,73 @@
+import axios from 'axios'
+
+export default class ErrorService {
+    constructor() {
+        // this.initHandler()
+    }
+
+    static onError(error) {
+        console.log("Heyo" + error)
+
+        // const response = error.response
+        // // console.log("Heyo")
+        // // console.log(error)
+
+        // if(response && response.status >= 400 && response.status < 405) {
+        //     // Handle HTTP errors which involve a bad request
+
+        // }
+
+        axios({
+            method: 'POST',
+            url: 'http://localhost:3000/logs',
+            data: {
+                error: error
+            }
+        })
+
+    }
+
+    static onWarn(error) {
+
+    }
+
+    static onInfo(error) {
+
+    }
+
+    // static onDebug(error) {
+    //     const response = error.response;
+    //     if (response && response.status >= 400 && response.status < 405) {
+    //       // You can handle this differently
+    //       this.sentryLogEngine(error);
+    //       return false;
+    //     }
+    //     // Send Error to Log Engine e.g LogRocket
+    //     this.logRocketLogEngine(error);
+    // }
+
+    // static initHandler() {
+    //     const scope = this;
+    //     window.onerror = (message, url, lineNo, columnNo, error) => {
+    //       console.log(error, "test");
+    //       if (error) {
+    //         scope.onError(error);
+    //         console.log(message, url, lineNo, columnNo, error);
+    //       }
+    //     };
+    // }
+
+    static displayErrorAlert(message) {
+        
+    }
+
+    // static logRocketLogEngine(error) {
+    //     // Implement LogRocket Engine here
+    //     console.log(error, "LogRocket");
+    //   }
+
+    // static sentryLogEngine(error) {
+    //     // Implement Sentry Engine here
+    //     console.log(error, "Sentry");
+    // }
+}
